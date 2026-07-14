47158820933855836c32c5c62389a7ba92892047
hyobin.roh <gyqls03097@gmail.com>
2026-07-15 07:23:12 +0900

    상태바, 네비바 가시성 수정

--- app/src/main/java/test/bccard/android/assignment/MainActivity.kt
+++ app/src/main/java/test/bccard/android/assignment/MainActivity.kt
@@ -1,7 +1,9 @@
 package test.bccard.android.assignment
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,7 +14,10 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+        )
 
         val di = AppDI(applicationContext)
 
